3ea3b29310b8a75e373a2caa341169f4026c6980
Educhin <c.s.arinze@gmail.com>
2020-03-04 18:49:08 -0500

    add nav-bar to all pages. This require adding bootstrap gem and a stylesheet. Additons to other files must be corrected.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
     before_action :authentication_required
-    
+
     def index 
         @courses = Course.all
     end
@@ -8,4 +8,16 @@ class CoursesController < ApplicationController
     def show
         @course = Course.find(params[:id])
     end
+
+    def enroll
+
+    end
+
+    # def ride
+    #     @attraction = Attraction.find(params[:attraction_id])
+    #     @user = User.find(params[:user_id])
+    #     @ride = Ride.create(attraction_id: @attraction.id, user_id: @user.id)
+    #     flash[:alert] = @ride.take_ride
+    #     redirect_to user_path(@user)
+    # end
 end
