1b0d8d90b19a8549354369f43b7664c02c8ceb04
Educhin <c.s.arinze@gmail.com>
2020-03-05 09:25:13 -0500

    add navbar links and logic to display login/logout and signin/myprofile contingent on if sessions[:student_id] is present

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+    def display_signup_or_profile
+        if session[:student_id]
+            student = Student.find(session[:student_id])
+            link_to "Your Profile", student_path(student), class: "navbar-brand"
+        else
+            link_to "Sign Up", new_student_path, class: "navbar-brand"
+        end
+    end
+
+    def display_signin_or_signout
+        if session[:student_id]
+            link_to "Log Out", logout_path, class: "navbar-brand"
+        else
+            link_to "Log In", login_path, class: "navbar-brand"
+        end
+    end
 end
